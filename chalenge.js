925a845d06a204a219eedc55293af114beb9620c
Kasaija Kenneth <kasaijak@gmail.com>
2022-10-12 16:31:04 +0300

    Complete exercise

--- chalenge.js
+++ chalenge.js
@@ -1,3 +1,4 @@
+// Task 1 before Modification/ be4 string reversal
 function stringLength(string) {
   let leng = string.length; 
   if(leng < 1){
@@ -5,6 +6,46 @@ function stringLength(string) {
   }else if(leng > 10){
     throw new Error('Expected length to be less than OR equal to 10')
   }else return leng;
-
 }
-module.exports = stringLength;
+
+//Task 1 Modified
+function reverseString(string) {
+  let reverseStr ='';
+  for (let index = string.length - 1; index >= 0; index--) {
+    reverseStr += string[index]; 
+  }
+  return reverseStr;
+}
+
+//Task 3 Calculator
+class Calculator {
+  constructor(firstNumber, secondNumber) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+  }
+  // Addition
+  static add = (firstNumber, secondNumber) => {
+    return firstNumber + secondNumber
+  }
+
+    //Subtraction
+  static subtract = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber
+  }
+
+    //Division
+  static divide = (firstNumber, secondNumber) => {
+    return firstNumber / secondNumber
+  }
+  
+    //Multiplication
+  static multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber
+  }
+}
+
+// Task 4: Captalize first Char of string
+function capitaliseString (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+module.exports = {stringLength, reverseString, Calculator, capitaliseString};
--- chalenge.test.js
+++ chalenge.test.js
@@ -1,13 +1,42 @@
 const { italic } = require('ansi-colors');
-const {stringLength} = require("./chalenge.js");
+const {stringLength, reverseString, Calculator, capitaliseString} = require("./chalenge.js");
 
+// Task 1 before modification/ be4 string reversal
   test('Testing for length less than 1', () => {
     expect(() => {stringLength('')}).toThrow(Error)});
   
   test('Testing for string length greater than 10', () => {
     expect(() => {stringLength('I love my Mother')}).toThrow(Error)});
   
-  test('Return string length', () => {
-    let str = stringLength('')
+  test('Return correct string length', () => {
+    let str = stringLength('Uganda')
     expect(str).toBe(6);
+  });
+
+  //Task 2 Modified
+  test('Reverse input string', () => {
+    let str =reverseString('Kenneth')
+    expect(str).toBe('htenneK');
+  });
+
+  // Task 3 Calculator
+  describe('Additon, Division, Subtraction, Multiplication', () => {
+    test('Add two numbers', () => {
+      expect(Calculator.add(4, 6)).toBe(10);
+    });
+    test('Subtract two numbers', () => {
+      expect(Calculator.subtract(15, 30)).toBe(-15);
+    });
+
+    test('Divide two numbers', () => {
+      expect(Calculator.divide(6, 3)).toBe(2);
+    });
+    test("should multiply two numbers", () => {
+      expect(Calculator.multiply(4, 100)).toBe(400);
+    });
+  });
+
+   // Task 4: Capitalise first Char of string function test
+  test('Capitalize first Char of string', () => {
+    expect(capitaliseString('home')).toBe('Home');
   });
